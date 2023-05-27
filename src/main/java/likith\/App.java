b1466e79b3118d7314335d2c076c3e233e91d2bf
root <root@ip-172-31-35-228.ap-south-1.compute.internal>
2023-05-27 13:26:26 +0000

    code

---
+++ src/main/java/likith\/App.java
@@ -0,0 +1,13 @@
+package likith\;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
